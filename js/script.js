449fb54f1cd4de1c28ee54e50c26a0c5e10504b3
Povrty <iakwan@s.sfusd.edu>
2019-10-08 01:07:21 +0000

    Works for vowel detection, needs work on consonants.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 $( "document" ).ready(function() {
 $('#button').click(function(){
-    var word = $('.input').val();
-    $(".output").text(word);
+    var text = $('.input').val();
+    var words = text.split(" ");
+    var vowels = ["a","e","i","o","u"];
+    var words1 = words[0].charAt(0)
+    var end1 ="";
+    if (vowels.includes(words1)) {
+        console.log("Correct")
+        $(".output").text(words[0]+"ay")
+    } else {
+        console.log("Wrong")
+        $(".output").text(words[0]+words1[0]+"ay")
+    }
 });
 });
